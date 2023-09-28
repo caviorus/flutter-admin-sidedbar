e4ea81edb55a4a7ce729a460fcdfb6d849eb1862
Caviorus <xlucief@gmail.com>
2023-09-28 20:16:24 +0700

    [add]SidebarContainer

--- lib/flutter_admin_sidebar.dart
+++ lib/flutter_admin_sidebar.dart
@@ -1,5 +1,37 @@
 library flutter_admin_sidebar;
 
-class Sidebar {
-    
+import 'package:flutter/material.dart';
+
+class SidebarContainer extends StatelessWidget {
+  final List<Widget> sidebarItem;
+  final Color? backgroundColor;
+  final double? width;
+  final Widget? content;
+  const SidebarContainer(
+      {super.key,
+      required this.sidebarItem,
+      this.backgroundColor,
+      this.width,
+      this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Material(
+          color: backgroundColor ?? Theme.of(context).primaryColor,
+          child: SizedBox(
+            width: width ?? 200,
+            height: MediaQuery.of(context).size.height,
+            child: ListView(
+              children: sidebarItem,
+            ),
+          ),
+        ),
+        Container(
+          child: content,
+        )
+      ],
+    );
+  }
 }
