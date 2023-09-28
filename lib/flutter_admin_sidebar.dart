256debdf627884394eb4a0f122e1e198f6afb1a6
Caviorus <xlucief@gmail.com>
2023-09-28 19:40:48 +0700

    initial commit

---
+++ lib/flutter_admin_sidebar.dart
@@ -0,0 +1,5 @@
+library flutter_admin_sidebar;
+
+class Sidebar {
+    
+}
---
+++ test/flutter_admin_sidebar_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_admin_sidebar/flutter_admin_sidebar.dart';
+
+void main() {
+  
+}
